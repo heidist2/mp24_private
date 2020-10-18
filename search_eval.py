87790a920e79ad75decc0af8ba7a29cb7f4b12e8
heidist2 <70455294+heidist2@users.noreply.github.com>
2020-10-18 16:58:38 -0700

    DP210

--- search_eval.py
+++ search_eval.py
@@ -13,7 +13,7 @@ def load_ranker(cfg_file):
     """
     #return metapy.index.PivotedLength(.35)
     #return metapy.index.OkapiBM25()
-    return metapy.index.DirichletPrior(mu=210)
+    return metapy.index.DirichletPrior(mu=215)
     #return metapy.index.JelinekMercer(1000)
 
 if __name__ == '__main__':
